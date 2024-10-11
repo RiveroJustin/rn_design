cd7d8902da7a079ee7732d70bb35c7f7c8d8521f
nojram <macbook@macbooks-MacBook-Pro.local>
2024-10-11 08:26:23 +0800

    hide the header

--- App.js
+++ App.js
@@ -3,18 +3,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './app/screens/LoginScreen';
 import AboutScreen from './app/screens/AboutScreen';
 import HomeScreen from './app/screens/HomeScreen';
-import { StatusBar } from 'react-native';
-import { header } from 'express/lib/request';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginScreen">
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="AboutScreen" component={AboutScreen} />
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
