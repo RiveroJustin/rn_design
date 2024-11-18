07c2201be4aa00a2d75cb7283fb4bb76a6e285fb
John Doe <johndoe@example.com>
2024-11-18 20:50:24 +0800

    added crud button

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="LoginScreen"
+        initialRouteName="HomeScreen"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
